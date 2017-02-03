251ae92fa45a3493e84cb7caa8d6c0918df77506
jonathanViralbamboo <jonathan@viralbamboo.com>
2017-02-03 10:45:19 +0000

    Refined Validation
    - Don’t run upload validation or send the attachment section of the
    email if the upload file is empty.

--- upload.php
+++ upload.php
@@ -3,48 +3,53 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+$fileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-// Check if image file is a actual document or fake document
-if(isset($_POST["submit"])) {
-  $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    $uploadOk = 1;
-  } else {
-    $error_message .= "File is must be a PDF or Word Document. ";
+// If no file was selected for upload, skip the upload validation.
+if (basename($_FILES["fileToUpload"]["name"]) != "")
+{
+  // Check if image file is a actual document or fake document
+  if(isset($_POST["submit"])) {
+    $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+      $uploadOk = 1;
+    } else {
+      $error_message .= "File is must be a PDF or Word Document. ";
+      $uploadOk = 0;
+    }
+  }
+
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    $error_message .= "File already exists. ";
     $uploadOk = 0;
   }
-}
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  $error_message .= "File already exists. ";
-  $uploadOk = 0;
-}
+  // Check file size
+  if ($_FILES["fileToUpload"]["size"] > 1000000) {
+    $error_message .= "Sorry, your file is too large. ";
+    $uploadOk = 0;
+  }
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 1000000) {
-  $error_message .= "Sorry, your file is too large. ";
-  $uploadOk = 0;
-}
+  // Allow certain file formats
+  if($fileType != "doc" && $fileType != "docx" && $fileType != "pdf") {
+    $error_message .= "Only DOC, DOCX & PDF files are allowed. ";
+    $uploadOk = 0;
+  }
 
-// Allow certain file formats
-if($imageFileType != "doc" && $imageFileType != "docx" && $imageFileType != "pdf") {
-  $error_message .= "Only DOC, DOCX & PDF files are allowed. ";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  $error_message .= "Sorry, your file was not uploaded. ";
-  // if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded. ";
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    $error_message .= "Sorry, your file was not uploaded. ";
+    // if everything is ok, try to upload file
   } else {
-    $error_message .= "Sorry, there was an error uploading your file. ";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded. ";
+    } else {
+      $error_message .= "Sorry, there was an error uploading your file. ";
+    }
   }
 }
+
 
 // Print out error messages (If there are any).
 echo $error_message;
@@ -95,13 +100,17 @@ echo $error_message;
     $body .= "Content-Transfer-Encoding: 8bit" . $eol;
     $body .= $message . $eol;
 
-    // attachment
-    $body .= "--" . $separator . $eol;
-    $body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
-    $body .= "Content-Transfer-Encoding: base64" . $eol;
-    $body .= "Content-Disposition: attachment" . $eol;
-    $body .= $content . $eol;
-    $body .= "--" . $separator . "--";
+    // If there was no file uploaded, don't send the attachment section.
+    if (basename($_FILES["fileToUpload"]["name"]) != "")
+    {
+      // attachment
+      $body .= "--" . $separator . $eol;
+      $body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
+      $body .= "Content-Transfer-Encoding: base64" . $eol;
+      $body .= "Content-Disposition: attachment" . $eol;
+      $body .= $content . $eol;
+      $body .= "--" . $separator . "--";
+    }
 
     mail($to,$subject,$body,$headers);
   }
